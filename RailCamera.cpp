ef8ff8939a6b96a7116d431cf96aebe67e30ecbf
大輝 池淵 <k022g1051@m.neec.ac.jp>
2023-06-12 15:27:12 +0900

    test

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -4,6 +4,7 @@
 void RailCamera::Initialize(WorldTransform worldTransform) { 
 	//ワールドトランスフォームの初期設定
 	//ワールド座標設定
+	worldTransform_.Initialize();
 	worldTransform_ = worldTransform;
 	//回転角(ラジアン)設定
 
