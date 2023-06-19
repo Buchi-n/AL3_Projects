763f652680fc595385496937e197b1a053df8cd1
大輝 池淵 <k022g1051@m.neec.ac.jp>
2023-06-19 13:57:21 +0900

    test

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -16,7 +16,7 @@ void RailCamera::Update() {
 	//ワールドトランスフォームの座標の数値を更新(移動)
 	worldTransform_.translation_.x += 0;
 	worldTransform_.translation_.y += 0;
-	worldTransform_.translation_.z += 0;
+	worldTransform_.translation_.z += 1;
 	//ワールドトランスフォームの角度の数値を更新(回転)
 	worldTransform_.rotation_.x += 0;
 	worldTransform_.rotation_.y += 0;
